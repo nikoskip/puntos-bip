3397804aaac3eb124cd7238780e2257cbc7d005b
Nikolas <nikoski89@hotmail.com>
2016-05-28 01:19:35 -0400

    Modifico el nombre de la App

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace PuntosBip\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use PuntosBip\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class PasswordController extends Controller
--- config/app.php
+++ config/app.php
@@ -151,10 +151,10 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        PuntosBip\Providers\AppServiceProvider::class,
+        PuntosBip\Providers\AuthServiceProvider::class,
+        PuntosBip\Providers\EventServiceProvider::class,
+        PuntosBip\Providers\RouteServiceProvider::class,
 
     ],
 
